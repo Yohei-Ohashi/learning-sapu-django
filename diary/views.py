a5ec22e72c651159310974369fdf587ce3fb831d
Yohei Ohashi <hashi.life@outlook.jp>
2025-12-28 23:59:53 +0900

    feat: 新規登録画面の作成についての学習

--- diary/views.py
+++ diary/views.py
@@ -1,14 +1,32 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+from .forms import PageForm
 
 
 class IndexView(View):
     def get(self, request):
-        datetime_now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y年%m月%d日 %H:%M:%S")
+        datetime_now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime(
+            "%Y年%m月%d日 %H:%M:%S"
+        )
         return render(request, "diary/index.html", {"datetime_now": datetime_now})
 
 
+class PageCreateView(View):
+    def get(self, request):
+        form = PageForm()
+        return render(request, "diary/page_form.html", {"form": form})
+
+    def post(self, request):
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("diary:index")
+        return render(request, "diary/page_form.html", {"form": form})
+
+
 index = IndexView.as_view()
+page_create = PageCreateView.as_view()

7eeb5f85b84578fbe5226b5b6ec68fda6825efb7
Yohei Ohashi <hashi.life@outlook.jp>
2026-01-03 22:08:48 +0900

    feat: 画像を登録できる様にする

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,7 +87,9 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        ),
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
@@ -116,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
--- diary/models.py
+++ diary/models.py
@@ -4,12 +4,17 @@ from django.db import models
 
 
 class Page(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID"
+    )
     title = models.CharField(max_length=100, verbose_name="タイトル")
     body = models.TextField(max_length=2000, verbose_name="本文")
     page_date = models.DateField(verbose_name="日付")
+    picture = models.ImageField(
+        upload_to="diary/picture/", blank=True, null=True, verbose_name="写真"
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
-    
+
     def __str__(self):
         return self.title
